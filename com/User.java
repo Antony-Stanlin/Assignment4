c730f2d125426fa2c4e52fbf3f994b3de5b95570
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:47:52 +0530

    Add files via upload

---
+++ com/User.java
@@ -0,0 +1,13 @@
+package com.methodOverloading.person;
+
+public class User {
+    public static void main(String[] args) {
+        Person person1 = new Person("Alice");
+        Person person2 = new Person("Bob", 30);
+        Person person3 = new Person("Charlie", 25, "123 Main St");
+
+        person1.displayDetails();
+        person2.displayDetails();
+        person3.displayDetails();
+    }
+}
