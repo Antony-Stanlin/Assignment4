4c3b14ef82551125becab2ab27bb39b2ccce8470
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:17:42 +0530

    Fighter.java

---
+++ com/inheritance/fighter/Fighter.java
@@ -0,0 +1,8 @@
+package com.inheritance.fighter;
+public abstract class Fighter {
+    public abstract boolean isVulnerable();
+    public abstract int getDamagePoints(Fighter opponent);
+
+  
+    public abstract String toString();
+}
