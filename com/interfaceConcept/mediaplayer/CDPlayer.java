a0cd632d1efe375b2d5d95d28806ad64414a2e59
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:32:08 +0530

    CDPlayer.java

---
+++ com/interfaceConcept/mediaplayer/CDPlayer.java
@@ -0,0 +1,19 @@
+package com.interfaceConcept.mediaplayer;
+
+public class CDPlayer implements Playable {
+    @Override
+    public void play() {
+        System.out.println("CD Player is now playing.");
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("CD Player is paused.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("CD Player has stopped.");
+    }
+} 
+
