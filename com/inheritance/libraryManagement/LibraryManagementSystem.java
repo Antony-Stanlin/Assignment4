a5885a7ed461ebccb65124c476c81ee5c2f2f250
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:19:16 +0530

    LibraryManagementSystem.java

---
+++ com/inheritance/libraryManagement/LibraryManagementSystem.java
@@ -0,0 +1,29 @@
+package com.inheritance.libraryManagement;
+
+public class LibraryManagementSystem {
+    
+    private String userType;
+    private String username;
+    private String password;
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void login() {
+    }
+
+    public void register() {
+    }
+
+    public void logout() {
+    }
+}
