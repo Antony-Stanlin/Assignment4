c5dda6cf33f0ec6ff3182d5b5a8f85a300b8f5f8
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:18:19 +0530

    Add files via upload

---
+++ com/inheritance/fighter/Wizard.java
@@ -0,0 +1,27 @@
+package com.inheritance.fighter;
+
+public class Wizard extends Fighter {
+    private boolean spellPrepared = false;
+
+    public void prepareSpell() {
+        spellPrepared = true;
+    }
+
+    
+    public boolean isVulnerable() {
+        return !spellPrepared; 
+    }
+
+    
+    public int getDamagePoints(Fighter opponent) {
+        if (spellPrepared) {
+            return 12; 
+        } else {
+            return 3; 
+        }
+    } 
+    public String toString() {
+        return "Fighter is a Wizard";
+    }
+
+}
