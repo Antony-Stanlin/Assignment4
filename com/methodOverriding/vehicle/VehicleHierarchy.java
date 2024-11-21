8f7f905966fcdc3602075ec665e577b2fef54d5d
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:56:25 +0530

    Add files via upload

---
+++ com/methodOverriding/vehicle/VehicleHierarchy.java
@@ -0,0 +1,15 @@
+package com.methodOverriding.vehicle;
+
+public class VehicleHierarchy {
+    public static void main(String[] args) {
+        Vehicle car = new Car("Toyota", "Corolla");
+        Vehicle motorcycle = new Motorcycle("Harley-Davidson", "Sportster");
+
+        car.start();
+        car.stop();
+        System.out.println();
+
+        motorcycle.start();
+        motorcycle.stop();
+    }
+}
