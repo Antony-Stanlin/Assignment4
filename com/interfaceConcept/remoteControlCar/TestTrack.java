934105d8f3e0f0458bae9d61ed9ce3c872cad1b7
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:35:47 +0530

    Add files via upload

---
+++ com/interfaceConcept/remoteControlCar/TestTrack.java
@@ -0,0 +1,15 @@
+package com.interfaceConcept.remoteControlCar;
+
+import java.util.List;
+import java.util.Collections;
+
+class TestTrack {
+    public static void race(RemoteControlCar car) {
+        car.drive();
+    }
+
+    public static List<ProductionRemoteControlCar> getRankedCars(List<ProductionRemoteControlCar> cars) {
+        Collections.sort(cars); 
+        return cars;
+    }
+}
