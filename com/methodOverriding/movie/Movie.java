ea2278feed70e1cc6b4d903a4e413c6556c5748d
Antony-Stanlin <antonystanlin0@gmail.com>
2024-11-21 19:53:51 +0530

    Movie.java

---
+++ com/methodOverriding/movie/Movie.java
@@ -0,0 +1,27 @@
+package com.methodOverriding.movie;
+
+public abstract class Movie {
+    private String title;
+    private String director;
+    private int releaseYear;
+
+    public Movie(String title, String director, int releaseYear) {
+        this.title = title;
+        this.director = director;
+        this.releaseYear = releaseYear;
+    }
+
+    public abstract void displayInfo();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+}
